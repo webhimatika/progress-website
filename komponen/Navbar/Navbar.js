06561982fc2a4e57e44455b2990d7552426ebeae
adib <adibhaidarzaky@gmail.com>
2024-10-15 14:00:09 +0700

    footer komponen redo

--- komponen/Navbar/Navbar.js
+++ komponen/Navbar/Navbar.js
@@ -67,12 +67,4 @@ window.addEventListener("DOMContentLoaded", () => {
     .catch((err) => {
       console.error(`Error fetching navigation bar: ${err}`);
     });
-  fetch("/komponen/Footer/Footer.html")
-    .then((response) => response.text())
-    .then((data) => {
-        document.getElementById("footer").innerHTML = data;
-    })
-    .catch((err) => {
-        console.error(`Error fetching footer: ${err}`);
-    });
 });
